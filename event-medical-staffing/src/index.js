5f3ca3d5c7ad5aca7df34203d1a4d4538f11ec67
Jack Larsen <jack@larsenwebdesign.com>
2022-01-13 04:17:30 -0700

    removed redundant render import

--- event-medical-staffing/src/index.js
+++ event-medical-staffing/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./reset.css";
 import "./fonts/fonts.css";
